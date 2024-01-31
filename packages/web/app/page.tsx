ca8bf1b00fef8942b5ca8f293f29c853090c52ca
jesreel01 <jesreelmiole93@gmail.com>
2024-01-31 09:44:02 +0800

    enhance

--- packages/web/app/page.tsx
+++ packages/web/app/page.tsx
@@ -8,10 +8,15 @@ import { FiTwitter } from "react-icons/fi";
 export default function Home() {
   return (
     <main className=" min-h-[100svh] flex-col bg-[#FAF4F0] py-20 px-5">
-      <div className="mx-auto max-w-3xl px-4 sm:px-6 xl:max-w-5xl xl:px-0 pt-6">
+      <div className="mx-auto max-w-3xl px-4 sm:px-6 xl:max-w-3xl xl:px-0 pt-6">
         <section className="mt-2 sm:mt-6 mb-12 flex flex-col items-center gap-x-14 xl:flex-row">
           <div>
-            <h1 className="text-4xl font-bold sm:text-6xl">Hi, I am Jesreel</h1>
+            <h1 className="text-4xl font-bold sm:text-5xl">
+              Hi,{" "}
+              <a href="/about" className=" hover:text-[#944ac2] transition">
+                I am Jesreel
+              </a>
+            </h1>
             <br />
             <h2 className="text-xl font-normal">
               Welcome to my personal blog, where I share my passion and
@@ -28,27 +33,37 @@ export default function Home() {
             </p>
 
             <div className="flex items-center  mt-4">
-              <p className="text-xl">Social Links:</p>
-              <div className="flex gap-3 ml-3 items-center">
-                <a target="_blank" href="https://github.com/jesreel01" className="hover:text-slate-600 hover:rotate-6 transition" >
+              <div className="flex gap-3  items-center">
+                <a
+                  target="_blank"
+                  href="https://github.com/jesreel01"
+                  className="hover:text-[#944ac2] hover:rotate-6 transition"
+                >
                   <FaGithub size={28} />
                 </a>
 
-                <a target="_blank" href="https://linkedin.com/in/jesreel-john-miole-ba2ba0215"  className="hover:text-slate-600 hover:rotate-6 transition">
+                <a
+                  target="_blank"
+                  href="https://linkedin.com/in/jesreel-john-miole-ba2ba0215"
+                  className="hover:text-[#944ac2] hover:rotate-6 transition"
+                >
                   <AiOutlineLinkedin size={34} />
                 </a>
                 <a href="https://twitter.com/imjesreel" target="_blank">
-                <FiTwitter size={30} className="hover:text-slate-600 hover:rotate-6 transition"/>
+                  <FiTwitter
+                    size={30}
+                    className="hover:text-[#944ac2] hover:rotate-6 transition"
+                  />
                 </a>
               </div>
             </div>
           </div>
-          <div className="flex items-center justify-center w-full sm:mt-5">
+          {/* <div className="flex items-center justify-center w-full sm:mt-5">
             <div className="grid grid-rows-3 gap-6 py-12 w-full max-w-96 text-center sm-12 sm:gap-8">
               <a
                 target="_blank"
                 // href=""
-                className="rounded-md border-2 bg-white border-black p-3 shadow-[4px_4px_0px_0px_rgba(0,0,0,1)] transition-all hover:translate-x-[3px] hover:translate-y-[3px] hover:shadow-none"
+                className="rounded-md border-2 bg-white border-black p-2 shadow-[4px_4px_0px_0px_rgba(0,0,0,1)] transition-all hover:translate-x-[3px] hover:translate-y-[3px] hover:shadow-none"
               >
                 Projects
               </a>
@@ -56,18 +71,23 @@ export default function Home() {
                 target="_blank"
                 // href=""
                 aria-disabled
-                className="rounded-md border-2 bg-white border-black p-3 shadow-[4px_4px_0px_0px_rgba(0,0,0,1)] transition-all hover:translate-x-[3px] hover:translate-y-[3px] hover:shadow-none"
+                className="rounded-md border-2 bg-white border-black p-2 shadow-[4px_4px_0px_0px_rgba(0,0,0,1)] transition-all hover:translate-x-[3px] hover:translate-y-[3px] hover:shadow-none"
               >
                 Blogs
               </a>
               <a
                 target="_blank"
-                className="rounded-md border-2  bg-white border-black p-3 shadow-[4px_4px_0px_0px_rgba(0,0,0,1)] transition-all hover:translate-x-[3px] hover:translate-y-[3px] hover:shadow-none"
+                className="rounded-md border-2  bg-white border-black p-2 shadow-[4px_4px_0px_0px_rgba(0,0,0,1)] transition-all hover:translate-x-[3px] hover:translate-y-[3px] hover:shadow-none"
               >
                 Resume
               </a>
             </div>
-          </div>
+          </div> */}
+        </section>
+        <section className="pt-4">
+          <h2 className="text-2xl font-bold sm:text-4xl">Recent Post</h2>
+          <br />
+          <hr className="border-gray-200" />
         </section>
       </div>
     </main>
--- packages/web/components/NavBar.tsx
+++ packages/web/components/NavBar.tsx
@@ -3,21 +3,21 @@ import { AiFillGithub } from "react-icons/ai";
 
 function Navbar({ title }: { title: string }) {
   return (
-    <nav className="fixed left-0 top-0 z-10 mx-auto flex h-20 w-full items-center border-b-2 border-black bg-[#FAF4F0] px-5 m500:h-16 ">
-      <div className="mx-auto flex w-full max-w-3xl px-4 sm:px-6 xl:max-w-5xl xl:px-0 justify-between">
+    <nav className="fixed left-0 top-0 z-10 mx-auto flex h-20  w-full items-center border-b-2 border-black bg-[#FAF4F0] px-5 m500:h-16 ">
+      <div className="mx-auto flex w-full max-w-3xl px-4 sm:px-6 xl:max-w-3xl xl:px-0 justify-between">
         {/* <MobileDrawer /> */}
 
-        <Link className="text-3xl font-bold m500:text-xl" href={"/"}>
+        <Link className="text-2xl font-bold m500:text-xl" href={"/"}>
           {title}
         </Link>
 
-        <a
+        {/* <a
           target="_blank"
           href="https://github.com/ekmas/neobrutalism-components"
           className="flex items-center justify-center rounded-md border-2 border-black p-2 shadow-[4px_4px_0px_0px_rgba(0,0,0,1)] transition-all hover:translate-x-[3px] hover:translate-y-[3px] hover:shadow-none"
         >
           <AiFillGithub className="h-6 w-6 m500:h-4 m500:w-4" />
-        </a>
+        </a> */}
       </div>
     </nav>
   );
