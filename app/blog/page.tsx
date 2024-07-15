d0316595dee0062bc3170528f6162e646752dca8
Jesreel <jesreelmiole93@gmail.com>
2024-07-15 09:57:59 +0800

    feat: add blog page
    - add components for the blog page

---
+++ app/blog/page.tsx
@@ -0,0 +1,18 @@
+import PostPreviewCard from "@/components/post-preview-card";
+import React from "react";
+
+const blog = () => {
+  return (
+    <div className="mt-8 ">
+      <h1 className="text-4xl font-medium tracking-wide">Blog</h1>
+      <div className="mt-6 space-y-8">
+        <PostPreviewCard />
+        <PostPreviewCard />
+        <PostPreviewCard />
+        <PostPreviewCard />
+      </div>
+    </div>
+  );
+};
+
+export default blog;
