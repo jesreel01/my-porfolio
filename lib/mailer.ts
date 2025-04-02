254f00d9ea9357e99a95834dee952abb6e8bf222
jesreel01 <jesreelmiole93@gmail.com>
2025-04-02 10:07:42 +0800

    Refactor code structure for improved readability and maintainability

--- lib/mailer.ts
+++ lib/mailer.ts
@@ -1,25 +1,25 @@
 import nodemailer from "nodemailer";
+import { secret } from "@aws-amplify/backend";
 
 export const transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
     user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
+    pass: process.env.EMAIL_PASS || (secret("EMAIL_PASS") as unknown as string),
   },
 });
 
 export const sendMail = async (name: string, userEmail: string, message: string) => {
   try {
-    console.log(process.env.EMAIL_USER);
     const info = await transporter.sendMail({
       from: `"${name}" <${process.env.EMAIL_USER}>`,
-      to: process.env.EMAIL_RECIPIENT, 
+      to: process.env.EMAIL_RECIPIENT,
       subject: `${name}`,
       text: `Name: ${name}\nEmail: ${userEmail}\nMessage: ${message}`,
       html: `<p><strong>Name:</strong> ${name}</p>
                <p><strong>Email:</strong> ${userEmail}</p>
                <p><strong>Message:</strong> ${message}</p>`,
-      replyTo: userEmail, 
+      replyTo: userEmail,
     });
 
     return info;
